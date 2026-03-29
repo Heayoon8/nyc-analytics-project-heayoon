1ea048d82e719ed7cadb342f2d14b71227550f5e
Heayoon Cha <tyg4567@gmail.com>
2026-03-29 22:28:50 +0000

    edit stg_nyc_open_restaurant_apps sql code

--- models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -1,9 +1,38 @@
-SELECT
-   LENGTH(CAST(incident_zip AS STRING)) AS zip_length,
-   incident_zip,
-   COUNT(*) AS count
- FROM `heayoon-cis9440.nyc_hw3_raw_data.source_dot_service_requests_history`
- WHERE incident_zip IS NOT NULL
-   AND LENGTH(CAST(incident_zip AS STRING)) != 5
- GROUP BY zip_length, incident_zip
- ORDER BY count DESC
+WITH source AS (
+    SELECT * 
+    FROM {{ source('raw', 'source_nyc_open_restaurant_apps') }}
+),
+
+cleaned AS (
+    SELECT
+        
+        CAST(objectid AS STRING) AS restaurant_id,
+
+        
+        CAST(restaurant_name AS STRING) AS restaurant_name,
+        CAST(legal_business_name AS STRING) AS legal_business_name,
+
+        
+        CAST(time_of_submission AS TIMESTAMP) AS time_of_submission,
+
+        
+        CASE
+            WHEN UPPER(TRIM(CAST(zip AS STRING))) IN ('N/A', 'NA') THEN NULL
+            WHEN LENGTH(CAST(zip AS STRING)) = 5 THEN CAST(zip AS STRING)
+            WHEN LENGTH(CAST(zip AS STRING)) = 9 THEN CAST(zip AS STRING)
+            ELSE NULL
+        END AS zip,
+
+        
+        CAST(borough AS STRING) AS borough,
+        CAST(latitude AS FLOAT64) AS latitude,
+        CAST(longitude AS FLOAT64) AS longitude,
+
+        
+        CURRENT_TIMESTAMP() AS _stg_loaded_at
+
+    FROM source
+    WHERE time_of_submission IS NOT NULL
+)
+
+SELECT * FROM cleaned
